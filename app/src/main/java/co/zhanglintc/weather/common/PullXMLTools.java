61560d9694b30943a79db589b25a72702741b718
Lane <zhanglintc623@gmail.com>
2015-11-04 22:29:12 +0800

    getText() => nextText()

--- app/src/main/java/co/zhanglintc/weather/common/PullXMLTools.java
+++ app/src/main/java/co/zhanglintc/weather/common/PullXMLTools.java
@@ -4,11 +4,8 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
-import java.io.IOError;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -30,15 +27,19 @@ public final class PullXMLTools {
      */
     public static Map<String,Object> parseXML(InputStream inputStream, String encoding, String language) throws Exception {
 
-        Map<String,Object> cityMap = new HashMap();
+        Map<String, Object> cityMap = new HashMap();
 
         // 创建XML解析工厂
         XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+
+        // 必须加上这句话, 否则 parser.setInput(inputStream, encoding); 会抛出异常 -- zhanglintc
+        factory.setNamespaceAware(true);
 
         // 获取XML解析类的引用
         XmlPullParser parser = factory.newPullParser();
 
         parser.setInput(inputStream, encoding);
+
 
         // 取得的语言ID
         String id = null;
@@ -46,7 +47,7 @@ public final class PullXMLTools {
         // 获取事件类型
         int eventType = parser.getEventType();
 
-        while(eventType!=XmlPullParser.END_DOCUMENT) {
+        while(eventType != XmlPullParser.END_DOCUMENT) {
             switch (eventType) {
                 case XmlPullParser.START_DOCUMENT:
 //                    if ("language".equals((parser.getName()))) {
@@ -60,11 +61,11 @@ public final class PullXMLTools {
                         id = parser.getAttributeValue(0);
                     } else
                     if (language.equals(id) && "city".equals((parser.getName()))) {
-                        cityMap.put("city", parser.getText());
+                        cityMap.put("city", parser.nextText());
                     } else if (language.equals(id) && "today".equals((parser.getName()))) {
-                        cityMap.put("today", parser.getText());
+                        cityMap.put("today", parser.nextText());
                     } else if (language.equals(id) && "tomorrow".equals((parser.getName()))) {
-                        cityMap.put("tomorrow", parser.getText());
+                        cityMap.put("tomorrow", parser.nextText());
                     }
                     break;
                 case XmlPullParser.END_TAG:
