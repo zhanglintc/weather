fb05c45d1d4e1f67f4ad9c354dca659c8a681e29
Lane <zhanglintc623@gmail.com>
2015-11-05 21:39:21 +0800

    sort the commitCounter by dict value

--- WeatherProjectReporter.py
+++ WeatherProjectReporter.py
@@ -2,26 +2,33 @@
 # -*- coding: utf-8 -*-
 
 import os, sys, re
+import python_send
 
+todayFile  = "today.txt"
+gitLogFile = "gitlog.txt"
+
+# return 3 lines as a string
 def read3Lines(line, f):
     for i in range(2):
         line += f.readline()
 
     return line
 
+# main function
 def main():
-    cd_command = "cd {} &&".format(sys.path[0])
-    os.system('{} git pull'.format(cd_command))
-    os.system('git log -100 > gitlog.txt')
+    cd_command = "cd {0} &&".format(sys.path[0])
+    os.system('{0} git pull'.format(cd_command))
+    os.system('git log -100 > {0}'.format(gitLogFile))
     
     # get date of today
-    os.system('date > today.txt')
-    fdate = open("today.txt")
-    today = fdate.read()[:10].replace(" ", "")
-    os.remove('today.txt')
+    os.system('date > {0}'.format(todayFile))
+    fdate = open(todayFile)
+    # today = fdate.read()[:10]
+    today = "Tue Nov  3"
+    os.remove(todayFile)
 
     # open gitlog
-    flog  = open("gitlog.txt")
+    flog  = open(gitLogFile)
 
     commitCounter = {}
     line = True
@@ -37,15 +44,20 @@ def main():
                 email = cmi.group(2)
                 date  = cmi.group(3)
 
-                if date.replace(" ", "") == today:
+                if date.replace(" ", "") == today.replace(" ", ""):
                     commitCounter[user] = commitCounter.get(user, 0) + 1
 
-    sendContent = ""
-    for key, val in commitCounter.items():
-        sendContent += ("{0}: {1}\n".format(key, val))
+    # convert commitCounter to list and make it sorted
+    commitCounter = sorted(commitCounter.items(), key = lambda d: d[1], reverse = True)
+
+    sendContent = "今日项目贡献排行({0}):\n\n".format(today)
+    idx = 1
+    for item in commitCounter:
+        sendContent += ("{0}. {1:12} {2} {3}\n".format(idx, item[0], item[1], "commit" if item[1] <= 1 else "commits"))
+        idx += 1
 
     print sendContent
-    os.remove("gitlog.txt")
+    os.remove(gitLogFile)
 
 if __name__ == '__main__':
     main()
