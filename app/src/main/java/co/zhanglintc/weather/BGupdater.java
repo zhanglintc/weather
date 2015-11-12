5ef6f3e842b57d4d20e6b38c543696b13c65a59c
Lane <zhanglintc623@gmail.com>
2015-11-12 11:01:12 +0800

    变量名整理, 格式整理

--- app/src/main/java/co/zhanglintc/weather/BGupdater.java
+++ app/src/main/java/co/zhanglintc/weather/BGupdater.java
@@ -16,17 +16,32 @@ import co.zhanglintc.weather.common.WeatherUtils;
 public class BGupdater extends Thread {
     Activity activity;
 
-    TextView city;
-    TextView todayTemp;
-    TextView todayStatus;
-    TextView systemTime;
+    TextView cityNameView;
+    TextView curTempCView;
+    TextView curDescView;
+    TextView tmrDateView;
+    TextView tmrTempCView;
+    TextView tmrDescView;
+    TextView datDateView;
+    TextView datTempCView;
+    TextView datDescView;
+    TextView sysTimeView;
 
-    ImageView imView;
-
-    Bitmap bmImg;
+    Bitmap curDescIcon;
+    ImageView curDescIconView;
 
     String rawJsonData;
+    String sysLang;
     String APIurl;
+
+    String curTempC;
+    String curDesc;
+    String cityName;
+
+    String tmrMaxTemp;
+    String tmrMinTemp;
+    String datMaxTemp;
+    String datMinTemp;
 
     BGupdater(Activity activity) {
         this.activity = activity;
@@ -37,41 +52,42 @@ public class BGupdater extends Thread {
     public void run() {
         Log.i("http", "Getting weather data...");
         httpHandler http = new httpHandler();
-        String s = http.get(APIurl);
-
-        final String temp_C;
-        final String weatherDesc;
-        final String iconURL;
-        final String systemLang;
-        final String cityName;
+        String retVal = http.get(APIurl);
 
         try {
-            systemLang = WeatherUtils.getLanguge();
-            WeatherParser we = new WeatherParser(s);
-            temp_C = we.getCurTemp_C();
-            cityName = we.getRequestCity();
-            if ("en".equals(systemLang)) {
-                weatherDesc = we.getCurWeatherDesc();
+            WeatherParser wp = new WeatherParser(retVal);
+
+            curTempC = wp.getCurTemp_C();
+            cityName = wp.getRequestCity();
+            tmrMaxTemp = wp.get2ndDayMaxTempC();
+            tmrMinTemp = wp.get2ndDayMinTempC();
+            datMaxTemp = wp.get3rdDayMaxTempC();
+            datMinTemp = wp.get3rdDayMinTempC();
+
+            curDescIcon  = WeatherUtils.returnBitMap(wp.getCurWeatherIconUrl());
+
+            sysLang = WeatherUtils.getLanguge();
+            if ("en".equals(sysLang)) {
+                curDesc = wp.getCurWeatherDesc();
             }
             else {
-                weatherDesc = we.getCurWeatherDescTranslation(systemLang);
+                curDesc = wp.getCurWeatherDescTranslation(sysLang);
             }
-            iconURL = we.getCurWeatherIconUrl();
-            bmImg  = WeatherUtils.returnBitMap(iconURL);
-            Log.i("http", "Current temperature: " + we.getCurTemp_C());
-            Log.i("http", "Current condition: " + weatherDesc);
-            Log.i("http", "Current weather icon URL: " + we.getCurWeatherIconUrl());
-            Log.i("http", "Requested city: " + we.getRequestCity());
+
+            Log.i("http", "Current temperature: " + wp.getCurTemp_C());
+            Log.i("http", "Current condition: " + curDesc);
+            Log.i("http", "Current weather icon URL: " + wp.getCurWeatherIconUrl());
+            Log.i("http", "Requested city: " + wp.getRequestCity());
             // 登录: http://www.worldweatheronline.com
             // 选择 FULL FORECAST 确认数据正确性
-            Log.i("http", "Tomorrow date: " + we.get2ndDayDate());
-            Log.i("http", "Tomorrow high: " + we.get2ndDayMaxTempC());
-            Log.i("http", "Tomorrow low: "  + we.get2ndDayMinTempC());
-            Log.i("http", "Tomorrow condition: "  + we.get2ndDayWeatherDesc());
-            Log.i("http", "Day after tomorrow date: " + we.get3rdDayDate());
-            Log.i("http", "Day after tomorrow high: " + we.get3rdDayMaxTempC());
-            Log.i("http", "Day after tomorrow low: "  + we.get3rdDayMinTempC());
-            Log.i("http", "Day after tomorrow condition: " + we.get3rdDayWeatherDesc());
+            Log.i("http", "Tomorrow date: " + wp.get2ndDayDate());
+            Log.i("http", "Tomorrow high: " + wp.get2ndDayMaxTempC());
+            Log.i("http", "Tomorrow low: "  + wp.get2ndDayMinTempC());
+            Log.i("http", "Tomorrow condition: "  + wp.get2ndDayWeatherDesc());
+            Log.i("http", "Day after tomorrow date: " + wp.get3rdDayDate());
+            Log.i("http", "Day after tomorrow high: " + wp.get3rdDayMaxTempC());
+            Log.i("http", "Day after tomorrow low: "  + wp.get3rdDayMinTempC());
+            Log.i("http", "Day after tomorrow condition: " + wp.get3rdDayWeatherDesc());
 
             activity.runOnUiThread(
                     new Runnable() {
@@ -80,23 +96,36 @@ public class BGupdater extends Thread {
                             activity.setContentView(R.layout.activity_main);
 
                             // TextView
-                            city = (TextView) activity.findViewById(R.id.city);
-                            todayTemp = (TextView) activity.findViewById(R.id.todayTemp);
-                            todayStatus = (TextView) activity.findViewById(R.id.todayDesc);
-                            systemTime = (TextView) activity.findViewById(R.id.systemTime);
+                            cityNameView = (TextView) activity.findViewById(R.id.cityName);
+                            curTempCView = (TextView) activity.findViewById(R.id.curTempC);
+                            curDescView = (TextView) activity.findViewById(R.id.curDesc);
+
+                            tmrDateView = (TextView) activity.findViewById(R.id.tmrDate);
+                            tmrTempCView = (TextView) activity.findViewById(R.id.tmrTempC);
+                            tmrDescView = (TextView) activity.findViewById(R.id.tmrDesc);
+                            datDateView = (TextView) activity.findViewById(R.id.datDate);
+                            datTempCView = (TextView) activity.findViewById(R.id.datTempC);
+                            datDescView = (TextView) activity.findViewById(R.id.datDesc);
+
+                            sysTimeView = (TextView) activity.findViewById(R.id.sysTime);
 
                             // ImageView
-                            imView = (ImageView) activity.findViewById(R.id.imview);
+                            curDescIconView = (ImageView) activity.findViewById(R.id.curDescIcon);
 
+                            // ------------------------------------------------------------------
 
                             // Set Text
-                            city.setText(cityName);
-                            todayTemp.setText(temp_C + "°C");
-                            todayStatus.setText(weatherDesc);
-                            systemTime.setText(WeatherUtils.getSystemTime());
+                            cityNameView.setText(cityName);
+                            curTempCView.setText(curTempC + "°C");
+                            curDescView.setText(curDesc);
+
+                            tmrTempCView.setText(tmrMinTemp + "~" + tmrMaxTemp + "°C");
+                            datTempCView.setText(datMinTemp + "~" + datMaxTemp + "°C");
+
+                            sysTimeView.setText(WeatherUtils.getSystemTime());
 
                             // Set Image
-                            imView.setImageBitmap(bmImg);
+                            curDescIconView.setImageBitmap(curDescIcon);
                         }
                     }
             );
