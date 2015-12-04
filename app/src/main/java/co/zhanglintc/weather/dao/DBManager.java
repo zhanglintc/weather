3d374e03040c0a0dc884a3085a53f06c36ad8f3d
Lane <zhanglintc623@gmail.com>
2015-12-04 21:52:36 +0800

    make DBHelper local

--- app/src/main/java/co/zhanglintc/weather/dao/DBManager.java
+++ app/src/main/java/co/zhanglintc/weather/dao/DBManager.java
@@ -13,14 +13,12 @@ import java.util.Date;
  * Created by yanbin on 2015/12/02.
  */
 public class DBManager {
-    private DBHelper helper;
+
     private SQLiteDatabase db;
 
     public DBManager(Context context) {
-        helper = new DBHelper(context);
-        //因为getWritableDatabase内部调用了mContext.openOrCreateDatabase(mName, 0, mFactory);
-        //所以要确保context已初始化,我们可以把实例化DBManager的步骤放在Activity的onCreate里
-        db = helper.getWritableDatabase();
+        DBHelper h = new DBHelper(context);
+        db = h.getWritableDatabase();
     }
 
     /**
